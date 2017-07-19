0d62b39c463192f9c5d9d0444488d0dd7629bb82
golden <goldensoftdev@gmail.com>
2017-07-19 20:14:08 +0200

    dev environment

---
+++ Gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var gsass = require('gulp-sass');
+var server = require('gulp-express');
+
+var paths = {
+    sass: ['sass/**/*.scss']
+};
+
+gulp.task('sass', function() {
+    gulp.src(paths.sass)
+        .pipe(gsass().on('error', gutil.log))
+        .pipe(gulp.dest('css/'));
+});
+
+gulp.task('default', ['sass']);
+
+gulp.task('watch', function() {
+    gulp.watch(paths.sass, ['sass']);
+})
+
+gulp.task('serve', function() {
+    gulp.run('default');
+    gulp.run('watch');
+});
